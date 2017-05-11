4f10df3d3c11eeab36f21e69076f358cfb5fac32
billywon <billy.y.won@gmail.com>
2017-05-10 18:39:58 -0700

    (feat) implements authentication using passport and bookshelf

--- server/passport.js
+++ server/passport.js
@@ -1,14 +1,72 @@
-const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcrypt');
 
-module.export = function(passport, LocalStrategy) {
+module.exports = function(passport, User) {
   passport.serializeUser((user, done) => {
+    console.log('============ serialize');
+    console.log(user);
+    console.log(user.id);
     done(null, user.id);
   });
 
   passport.deserializeUser((id, done) => {
-
+    console.log('============ deserialize');
+    new User({ 'id': id }).fetch().then((model) => {
+      console.log('============ here?', model.get('username'));
+      done(null, model);
+    }).catch((error) => {
+      console.log('============ or here?');
+      done(error, null);
+    });
   });
 
+  passport.use('local-signup', new LocalStrategy({
+    usernameField: 'username',
+    passwordField: 'password'
+  }, (username, password, done) => {
+    console.log(username);
+    console.log(password);
+    new User({ username: username }).fetch().then((model) => {
+      if (model) {
+        done(null, false);
+      } else {
+        console.log('is it here?: first');
+        bcrypt.hash(password, 10, (err, hash) => {
+          User.forge({
+            username: username,
+            password: hash
+          }).save().then((user) => {
+            console.log('is it here?: ', user);
+            done(null, user);
+          }).catch((error) => {
+            console.log('is it error?: ', error);
+            done(error, null);
+          });
+        });
+      }
+    });
+  }));
+
+  passport.use('local', new LocalStrategy({
+    usernameField: 'username',
+    passwordField: 'password'
+  }, (username, password, done) => {
+    console.log(username);
+    console.log(password);
+    new User({ username: username }).fetch().then((model) => {
+      if (!model) {
+        done(null, false);
+      } else {
+        console.log('is it here?: first');
+        bcrypt.compare(password, model.get('password'), (err, results) => {
+          if (results) {
+            done(null, model);
+          } else {
+            done(null, false);
+          }
+        });
+      }
+    });
+  }));
 
 };
--- server/server.js
+++ server/server.js
@@ -1,35 +1,31 @@
 const express = require('express');
-const db = require('./db/db');
 const path = require('path');
+const bcrypt = require('bcrypt');
+const db = require('./db/db');
+const passport = require('passport');
+const session = require('express-session');
+const bodyParser = require('body-parser');
 const User = db.Model.extend({ tableName: 'user' });
 const Picture = db.Model.extend({ tableName: 'picture' });
 const Scoreboard = db.Model.extend({ tableName: 'scoreboard' });
-
-const bodyParser = require('body-parser');
 
 const PORT = 8000;
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(`${__dirname}/../app/dist`));
+app.use(session({ secret: 'there is no secret' }));
+app.use(passport.initialize());
+app.use(passport.session());
+require('./passport')(passport, User);
 
-app.route('/Signup').post((req, res) => {
+app.route('/Signup').post(passport.authenticate('local-signup'), (req, res) => {
   console.log(req.body);
-  new User({ username: req.body.username }).fetch().then((model) => {
-    console.log('fetching the data');
-    console.log(model.get('username'));
-    res.status(500).send('user already exists');
-  }).catch(() => {
-    User.forge({
-      username: req.body.username,
-      password: req.body.password
-    }).save().then((user) => {
-      res.json({ error: false, data: { id: user.get('id') } });
-    }).catch((error) => {
-      res.status(500).json({ error: true, data: { error: error }});
-    });
-  });
+  res.send('hello world!');
 });
 
+app.route('/Signin').post(passport.authenticate('local'), (req, res) => {
+  res.send('welcome back!');
+});
 
 app.route('*').get((req, res) => {
   res.sendFile(path.resolve(__dirname, '../app/dist', 'index.html'));
